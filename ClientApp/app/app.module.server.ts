f3d5f33c43321d88de18083535046c83208e87c2
Rodion Zinovyev <rodion.zinovyev@gmail.com>
2017-06-08 18:08:17 +0300

    ok

--- ClientApp/app/app.module.server.ts
+++ ClientApp/app/app.module.server.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { sharedConfig } from './app.module.shared';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 
 @NgModule({
     bootstrap: sharedConfig.bootstrap,
     declarations: sharedConfig.declarations,
     imports: [
         ServerModule,
-        BrowserModule,
-        FormsModule,
-        HttpModule,
         ...sharedConfig.imports
-    ],
-
-    providers: [
-        { provide: 'ORIGIN_URL', useValue: location.origin }
     ]
 })
 export class AppModule {
---
+++ ClientApp/app/app.module.shared.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+export const sharedConfig: NgModule = {
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,      
+        HomeComponent
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+          
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+};
